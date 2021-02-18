1928af9e3049a1ac335b66411acd40c204f720c4
andreabis <andreadelucia@hotmail.com>
2021-02-18 12:06:33 +0100

    added first draft

---
+++ test_cpu_usage.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+case "$1" in 
+start)
+   /usr/local/bin/test_cpu_usage.sh & echo $!>/var/run/hit.pid   
+   #while true; do
+   top -bn 1 | head -n 8 | tail -n 1 >> /var/log/test_cpu_usage/log.txt
+   sleep 10
+   #echo ""
+   #done
+   
+   ;;
+stop)
+   kill `cat /var/run/hps aux | grep -i it.pid`
+   rm /var/run/hit.pid
+   ;;
+restart)
+   $0 stop
+   $0 start
+   ;;
+status)
+   if [ -e /var/run/hit.pid ]; then
+      echo hit.sh is running, pid=`cat /var/run/hit.pid`
+   else
+      echo hit.sh is NOT running
+      exit 1
+   fi
+   ;;
+*)
+   echo "Usage: $0 {start|stop|status|restart}"
+esac
+
+exit 0 
